b78a4cb74d9342e3aefcb868871d7e8e57ffe1bd
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2025-12-22 14:45:33 +0100

    advanced task 2

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,29 +3,31 @@ import Button from '@/components/Button';
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=' text-xl font-extralight'>Landing Page</h1>
+    <div className='p-4'>
+      <h1 className='text-xl font-extralight mb-4'>Landing Page</h1>
 
-      {/* Small Buttons */}
+      {/* SMALL BUTTONS */}
       <div className='flex gap-3 mb-4'>
         <Button title='Small' className='text-sm rounded-sm' />
         <Button title='Small' className='text-sm rounded-md' />
-        <Button title='Small' className='text-sm rounded=full' />
+        <Button title='Small' className='text-sm rounded-full' />
       </div>
 
-      {/* Medium Buttons */}
-      <div className='flex gap-3 mb-6'>
+      {/* MEDIUM BUTTONS */}
+      <div className='flex gap-3 mb-4'>
         <Button title='Medium' className='text-base rounded-sm' />
         <Button title='Medium' className='text-base rounded-md' />
-        <Button title='Medium' className='text-base rounded=full' />
+        <Button title='Medium' className='text-base rounded-full' />
       </div>
 
-      {/* Card Component */}
+      {/* LARGE BUTTONS */}
       <div className='flex gap-3 mb-6'>
-        <Button title='Large' className='text-lg px-6 rounded-sm' />
-        <Button title='Large' className='text-lg px-6 rounded-md' />
-        <Button title='Large' className='text-lg px-6 rounded=full' />
+        <Button title='Large' className='text-lg px-6 py-3 rounded-sm' />
+        <Button title='Large' className='text-lg px-6 py-3 rounded-md' />
+        <Button title='Large' className='text-lg px-6 py-3 rounded-full' />
       </div>
+
+      {/* CARD COMPONENT */}
       <Card />
     </div>
   );
